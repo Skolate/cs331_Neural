d7f84616641614fcb0b223eb368c2d203f7c1111
Skolate <nath2117@sky.com>
2019-04-01 12:02:34 +0100

    Implemented Roland's code in python, draws necessary graphs

--- rolands_way.py
+++ rolands_way.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
 import random
+from matplotlib import pyplot as plt
 
 #   ---- ROLAND'S FUNCTIONS ----
 
@@ -31,52 +32,120 @@ def htout(n):
     return table
 
 
-
-input_size = 16
-hidden_layers = 8
 input_default = [[0, 0], [0, 1], [1, 0], [1, 1]]
-normal_x = []
-offset = []
 
-for x in range(int(input_size/4)):
-    normal_x.extend(input_default)
+def generate_inputs(input_size):
+    normal_x = []
+    offset = []
 
-for x in range(input_size):
-    offset.append([-1])
+    for x in range(int(input_size/4)):
+        normal_x.extend(input_default)
 
-#   Generates the random inputs and the correct results for said inputs
-Y = np.remainder(np.sum(normal_x, axis=1), 2)
-#   Generates noise, the scale is standard deviation, so for variance of 1/4 a scale if 1/2 is required, I think.
-noise = np.random.normal(loc=0, scale=0.5, size=(input_size, 2))
-#   Adding noise
-X = normal_x + noise
-#   Adding offset as 3rd input
-X = np.append(X, offset, axis=1)
+    for x in range(input_size):
+        offset.append([-1])
 
-X = np.transpose(X)
+    #   Generates he correct results for said inputs
+    Y = np.remainder(np.sum(normal_x, axis=1), 2)
+    #   Generates noise, the scale is standard deviation, so for variance of 1/4 a scale if 1/2 is required, I think.
+    noise = np.random.normal(loc=0, scale=0.5, size=(input_size, 2))
+    #   Adding noise
+    X = normal_x + noise
+    #   Adding offset as 3rd input
+    X = np.append(X, offset, axis=1)
+    X = np.transpose(X)
+
+    return X, Y
+
+
+#   Roland's training data, useful for checking if the algorithm produces similar results and is thus correctly implemented
+# X=[[-0.310691, -0.309003, 1.25774, 1.31959, -0.0897083, -0.457115,
+# 1.42524, 1.43962, -0.21377, -0.16744, 0.579612, 1.90558, 0.442017,
+# 0.204012, 1.75664, 0.584128],
+# [0.0164278, 0.898471, -0.231735, 0.82952, -1.02045, 1.84369, 0.111823,
+# 0.28365, 0.0759174, 0.985518, 0.584378, 0.434351, 0.35245, -0.0194183,
+# -0.336488, 1.45608], [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+# -1, -1, -1, -1]]
+
+
 
 #   Roland's method from mathmatica
 
-weight_1 = intoh(2)
-weight_2 = htout(2)
-
-error_table = np.zeros(1000)
-offset_vector = np.full((1, 16), -1)
-
-
-for loop in range(1000):
+def predict(weight_1, weight_2, X):
+    offset_vector = np.full((1, len(X[0])), -1)
     hid = logistic_function( np.dot( weight_1, X ) )
     out = logistic_function( np.dot( weight_2, np.concatenate( (hid, offset_vector) ) ) )
-    error = Y - out
-    error_table[loop] = np.mean(error*error)
-    delta_out = error * out * (1-out)
-    error_hid = np.outer(weight_2, delta_out)[0:1, :]
-    delta_hid = error_hid * hid * (1-hid)
-    weight_2 += np.dot( delta_out, np.transpose( np.concatenate( (hid, offset_vector) ) ) )
-    weight_1 += np.dot( delta_hid, np.transpose( X ) )
-    if loop % 100 == 0:
-        print (error_table[loop])
+    return out
 
-print (error_table)
+def neural_net(hidden_layers, X, Y):
+    weight_1 = intoh(hidden_layers)
+    weight_2 = htout(hidden_layers)
+
+    error_table = np.zeros(1000)
+    offset_vector = np.full((1, len(X[0])), -1)
+
+    #   Implementation of Roland's mathmatica neural net
+    for loop in range(1000):
+        hid = logistic_function( np.dot( weight_1, X ) )
+        out = logistic_function( np.dot( weight_2, np.concatenate( (hid, offset_vector) ) ) )
+        error = Y - out
+        error_table[loop] = np.mean(error*error)
+        delta_out = error * out * (1-out)
+        error_hid = np.outer(weight_2, delta_out)[0:1, :]
+        delta_hid = error_hid * hid * (1-hid)
+        weight_2 += np.dot( delta_out, np.transpose( np.concatenate( (hid, offset_vector) ) ) )
+        weight_1 += np.dot( delta_hid, np.transpose( X ) )
+        if loop % 100 == 0:
+            print (error_table[loop])
+
+    #   Plotting line graph
+    global plot_count
+    global input_count
+    plt.figure(input_count)
+    plt.subplot(1, 3, plot_count)
+    plt.plot(error_table)
+    plt.axis([0, 1000, 0, 1])
+    plt.title('Hidden Layer Size: ' + str(hidden_layers) + '   Input Size: ' + str(len(X[0])))
+
+    #   Plotting output mapping
+    X_1 = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
+    X_1 = np.arange(0, 1.05, 0.05)
+    output_map = np.zeros((len(X_1), len(X_1)))
+    for i in range(len(X_1)):
+        for j in range(len(X_1)):
+            output_map[i, j] = predict(weight_1, weight_2, [[X_1[i]], [X_1[j]], [-1]])
+    plt.figure(input_count + 1)
+    plt.subplot(1, 3, plot_count)
+    plt.imshow(output_map, cmap='gray', origin='lower')
+    plt.title('Hidden Layer Size: ' + str(hidden_layers) + '   Input Size: ' + str(len(X[0])))
+
+    return weight_1, weight_2
+
+
+def net_error(weight_1, weight_2, X, Y):
+    out = predict(weight_1, weight_2, X)
+    error = Y - out
+    mse = np.mean(error*error)
+    return mse
+    
+#   Generating testing data for the neural nets
+X_test, Y_test = generate_inputs(64)
+
+plot_count = 1
+input_count = 1
+same_inputs = True
+for input_size in [16, 32, 64]:
+    plot_count = 1
+    if same_inputs == True:
+        X, Y = generate_inputs(input_size)
+    for hidden_layers in [2, 4, 8]:
+        if same_inputs == False:
+            X, Y = generate_inputs(input_size)
+
+        weight_1, weight_2 = neural_net(hidden_layers, X, Y)
+        mse = net_error(weight_1, weight_2, X_test, Y_test)
+        print ('test set error: ' + str(mse))
+        plot_count += 1
+    input_count += 2
+plt.show()
 
 
